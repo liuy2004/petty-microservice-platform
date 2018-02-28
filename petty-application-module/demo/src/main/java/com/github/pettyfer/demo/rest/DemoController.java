3f05e61bfbe91df0fc6e9d1b30db3ce7c5bf842b
pettyferlove@live.cn <pettyferlove@live.cn>
2018-02-28 11:09:35 +0800

    oauth2 jwt用户验证服务

---
+++ petty-application-module/demo/src/main/java/com/github/pettyfer/demo/rest/DemoController.java
@@ -0,0 +1,21 @@
+package com.github.pettyfer.demo.rest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "demo")
+public class DemoController {
+    @RequestMapping(value = "get",method = RequestMethod.GET)
+    public String get(){
+        return "Demo";
+    }
+
+    @GetMapping("/user")
+    public Authentication user(Authentication authentication) {
+        return authentication;
+    }
+}
