48e44f9647e9025b0b261f8745738a79157dc9dc
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-01 14:26:26 +0800

    Revert "oauth2 jwt用户验证服务"
    This reverts commit 8d5e55d

--- petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/adapter/PettyAuthorizationAdapter.java
+++ petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/adapter/PettyAuthorizationAdapter.java
@@ -18,11 +18,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @author Petty
@@ -49,16 +46,6 @@ public class PettyAuthorizationAdapter extends AuthorizationServerConfigurerAdap
     @Autowired
     private ResponseExceptionTranslator responseExceptionTranslator;
 
-    @Autowired
-    private AuthorizationEndpoint authorizationEndpoint;
-
-    @PostConstruct
-    public void init() {
-        authorizationEndpoint.setUserApprovalPage("forward:/oauth/my_approval_page");
-        authorizationEndpoint.setErrorPage("forward:/oauth/my_error_page");
-    }
-
-
     /**
      * 配置客户端信息
      *
@@ -72,7 +59,7 @@ public class PettyAuthorizationAdapter extends AuthorizationServerConfigurerAdap
                 .secret(authServerConfig.getClientSecret())
                 .authorizedGrantTypes(SecurityConstant.REFRESH_TOKEN, SecurityConstant.PASSWORD, SecurityConstant.AUTHORIZATION_CODE, SecurityConstant.CLIENT)
                 .scopes(authServerConfig.getScope())
-                .autoApprove(false);
+                .autoApprove(true);
     }
 
     @Override
