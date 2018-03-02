2bbd71c230dc842b4aa0c614b652dba9a5460c1f
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-02 09:15:56 +0800

    Revert "oauth2 jwt用户验证服务"
    This reverts commit 8d5e55d

--- petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/adapter/PettyResourceServerAdapter.java
+++ petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/adapter/PettyResourceServerAdapter.java
@@ -2,11 +2,18 @@ package com.github.pettyfer.basic.auth.adapter;
 
 import com.github.pettyfer.basic.common.config.FilterUrlsPropertiesConifg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 
 /**
@@ -17,6 +24,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @Configuration
 @EnableResourceServer
 public class PettyResourceServerAdapter extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
 
     @Autowired
     private FilterUrlsPropertiesConifg filterUrlsPropertiesConifg;
@@ -31,6 +41,15 @@ public class PettyResourceServerAdapter extends ResourceServerConfigurerAdapter 
         registry.anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Primary
+    @Bean
+    public ResourceServerTokenServices resourceServerTokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(new RedisTokenStore(redisConnectionFactory));
+        tokenServices.setSupportRefreshToken(true);
+        return tokenServices;
     }
 
 }
