1fce4dd393c4df65884e5fa0940ce981115d767d
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-04 16:21:08 +0800

    网关限流

--- petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/PettyAuthServiceApplication.java
+++ petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/PettyAuthServiceApplication.java
@@ -3,10 +3,13 @@ package com.github.pettyfer.basic.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 认证服务&资源服务
@@ -22,5 +25,11 @@ public class PettyAuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PettyAuthServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
