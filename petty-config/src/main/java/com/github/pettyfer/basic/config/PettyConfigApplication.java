b749e2630b921b71acab507848b883c895d62479
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-08 20:02:45 +0800

    服务配置中心

---
+++ petty-config/src/main/java/com/github/pettyfer/basic/config/PettyConfigApplication.java
@@ -0,0 +1,12 @@
+package com.github.pettyfer.basic.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettyConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettyConfigApplication.class, args);
+	}
+}
---
+++ petty-config/src/test/java/com/github/pettyfer/basic/config/PettyConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pettyfer.basic.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PettyConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
