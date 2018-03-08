587952d9aa4cd91ebe64f77ce5965e885f875a70
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-08 21:32:22 +0800

    监控中心

--- petty-monitor/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorApplication.java
+++ petty-monitor/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorApplication.java
@@ -1,14 +1,16 @@
-package com.github.pettyfer.basic.config;
+package com.github.pettyfer.basic.monitor;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAdminServer
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PettyConfigApplication {
+public class PettyMonitorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PettyConfigApplication.class, args);
+		SpringApplication.run(PettyMonitorApplication.class, args);
 	}
 }
