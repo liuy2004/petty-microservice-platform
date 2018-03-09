361b0998ad5a4db7fedce56f49cedc11780c31a9
Petty <liuyang@minstone.com.cn>
2018-03-09 17:37:27 +0800

    Spring Cloud Config

--- petty-monitor/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorApplication.java
+++ petty-monitor/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableTurbine
 public class PettyMonitorApplication {
 
 	public static void main(String[] args) {
