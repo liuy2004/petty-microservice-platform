3005dbdf1aefd588962dcec588c274ed2e5dfe57
Petty <liuyang@minstone.com.cn>
2018-03-09 09:52:31 +0800

    Spring Cloud Config

--- petty-config/src/main/java/com/github/pettyfer/basic/config/PettyConfigApplication.java
+++ petty-config/src/main/java/com/github/pettyfer/basic/config/PettyConfigApplication.java
@@ -3,7 +3,9 @@ package com.github.pettyfer.basic.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PettyConfigApplication {
