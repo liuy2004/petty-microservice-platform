3ae398cfdf5571b875f21c65de7999162aa7a05b
Petty <liuyang@minstone.com.cn>
2018-03-09 14:05:52 +0800

    Spring Cloud Config

--- petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/PettyAuthServiceApplication.java
+++ petty-auth-service/src/main/java/com/github/pettyfer/basic/auth/PettyAuthServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,7 +18,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
-@EnableHystrixDashboard
 @PettyConfigure
 public class PettyAuthServiceApplication {
 
