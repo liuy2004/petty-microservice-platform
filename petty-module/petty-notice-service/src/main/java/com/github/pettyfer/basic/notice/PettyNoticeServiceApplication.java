c01759cb471b3aecea49f25a53f9d8f134311524
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-10 09:11:40 +0800

    监控中心

---
+++ petty-module/petty-notice-service/src/main/java/com/github/pettyfer/basic/notice/PettyNoticeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.pettyfer.basic.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettyNoticeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettyNoticeServiceApplication.class, args);
+	}
+}
