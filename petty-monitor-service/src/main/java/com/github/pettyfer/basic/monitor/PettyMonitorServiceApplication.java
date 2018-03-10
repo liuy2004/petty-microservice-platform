ada33459b6c4f045b09911e3c04a4da6c264bd9b
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-10 19:35:24 +0800

    监控中心

--- petty-monitor-service/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorServiceApplication.java
+++ petty-monitor-service/src/main/java/com/github/pettyfer/basic/monitor/PettyMonitorServiceApplication.java
@@ -1,10 +1,6 @@
 package com.github.pettyfer.basic.monitor;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
-import de.codecentric.boot.admin.notify.LoggingNotifier;
-import de.codecentric.boot.admin.notify.Notifier;
-import de.codecentric.boot.admin.notify.RemindingNotifier;
-import de.codecentric.boot.admin.notify.filter.FilteringNotifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,12 +9,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.TimeUnit;
 
 @EnableAdminServer
 @SpringBootApplication
