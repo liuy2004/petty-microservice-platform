4cde623067fc60ab707ace8dde0ed44084dafa6b
pettyferlove@live.cn <pettyferlove@live.cn>
2018-03-28 10:10:08 +0800

    代码可读性提升

--- petty-common/src/main/java/com/github/pettyfer/basic/common/model/GroupInfo.java
+++ petty-common/src/main/java/com/github/pettyfer/basic/common/model/GroupInfo.java
@@ -1,11 +1,24 @@
 package com.github.pettyfer.basic.common.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * 用户组
+ *
  * @author Petty
  */
-public class GroupInfo implements Serializable{
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupInfo implements Serializable {
     private static final long serialVersionUID = 4276507548530306822L;
+    private String groupId;
+    private String groupName;
+    private String parentId;
+    private BigDecimal groupType;
 }
--- petty-common/src/main/java/com/github/pettyfer/basic/common/model/RoleInfo.java
+++ petty-common/src/main/java/com/github/pettyfer/basic/common/model/RoleInfo.java
@@ -1,11 +1,23 @@
 package com.github.pettyfer.basic.common.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
  * 角色信息模型
+ *
  * @author Petty
  */
-public class RoleInfo implements Serializable{
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleInfo implements Serializable {
     private static final long serialVersionUID = 4994910569599395548L;
+    private String roleId;
+    private String roleName;
+    private String roleType;
 }
