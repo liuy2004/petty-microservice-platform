da867b61254a652306ba6e20e93c7f58d2c758d3
pettyferlove@live.cn <pettyferlove@live.cn>
2018-04-26 09:06:56 +0800

    Api权限过滤

--- petty-common/src/main/java/com/github/pettyfer/basic/common/model/auth/Role.java
+++ petty-common/src/main/java/com/github/pettyfer/basic/common/model/auth/Role.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role implements Serializable {
+    private static final long serialVersionUID = 3506075171647853639L;
     private String roleId;
     private String roleName;
     private String roleType;
